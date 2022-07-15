f5958748df51944ef91d7ac4e6adbea39c81eac2
Vadym.Kosin <Vadym.Kosin@tu-dortmund.de>
2022-07-15 14:50:50 +0200

    changed Day5, Day05 works correctly now

--- src/main/kotlin/Day05.kt
+++ src/main/kotlin/Day05.kt
@@ -42,11 +42,15 @@ class Day05 : Puzzle<Int>("Day05", 5, 12) {
     }
 
     private fun diffOfTwo(p1: Pair<Int, Int>, p2: Pair<Int, Int>): Int {
-        if (p1.first == p2.first) {
-            return p2.second - p1.second
+        return if (p1.first == p2.first) {
+            p2.second - p1.second
         } else {
-            return p2.first - p1.first
+            p2.first - p1.first
         }
+    }
+
+    private fun diffOfTwoDiagonals(p1: Pair<Int, Int>, p2: Pair<Int, Int>): Pair<Int, Int> {
+        return (Pair(p2.first - p1.first, p2.second - p1.second))
     }
 
 
@@ -61,28 +65,27 @@ class Day05 : Puzzle<Int>("Day05", 5, 12) {
             it.split(" -> ").map(f).zipWithNext().first()
         }
             .filter { (p1, p2) ->
-                p1.first == p1.second && p2.first == p2.second ||
-                        (p1.first == p2.second && p1.second == p2.first)
+                (p1.first - p2.first) * (p1.first - p2.first) == (p1.second - p2.second) * (p1.second - p2.second)
             }
 
         for (i in inputInList) {
-            var diff = diffOfTwo(i.first, i.second)
-
+            var diff = diffOfTwoDiagonals(i.first, i.second)
             mapOfSmoke.merge(i.first, 1, Int::plus)
             mapOfSmoke.merge(i.second, 1, Int::plus)
 
-            while (diff != 1 && diff != -1) {
-                diff = if (diff < 0) diff + 1 else diff - 1
+            while (diff.first != 1 && diff.first != -1) {
+                diff = if (diff.first < 0) Pair(diff.first + 1, diff.second) else Pair(diff.first - 1, diff.second)
+                diff = if (diff.second < 0) Pair(diff.first, diff.second + 1) else Pair(diff.first, diff.second - 1)
 
-                if(i.first.first == i.first.second && i.second.first == i.second.second){
-                val toAd = Pair(i.first.first + diff, i.first.second + diff)
+                val toAd = Pair(i.first.first + diff.first, i.first.second + diff.second)
+
                 mapOfSmoke.merge(toAd, 1, Int::plus)
-
-                }
             }
 
         }
+
         return mapOfSmoke.count { it.value >= 2 }
     }
+
 
 }
