a95f4418f3495fa8c6484978c4c1828aff318231
Vadym.Kosin <Vadym.Kosin@tu-dortmund.de>
2022-08-01 22:41:13 +0200

    added the most horrific and janky solution to Day08 anyone has ever witnessed

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,7 @@
 import kotlin.system.measureTimeMillis
 
 fun main() {
-    val puzzle = Day07()
+    val puzzle = Day08()
     val testInput = readInput("${puzzle.name}_test")
     var result: Any
     println(
